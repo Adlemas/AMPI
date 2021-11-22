1551c3570d8f547d7c1d9a19e33ba5834d373311
LandaMeen <princ228321@gmail.com>
2021-11-22 12:41:39 +0300

    testing sleep function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#ifndef _WIN32
+#include "windows.h"
+#else
+#include "unistd.h"
+#endif
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello" << std::endl;
+    std::cout.flush();
+    sleep(5);
+    std::cout << " World!" << std::endl;
     return 0;
 };
